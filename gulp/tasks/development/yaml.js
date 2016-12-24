bca507229bdfa6a5388679b26e02a571506019fa
diogorusso <hello@diogorusso.com>
2016-12-23 23:36:12 -0800

    Update 2016-12-24T07:29:39.031Z

--- gulp/tasks/development/yaml.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var yamlMerge = require('gulp-yaml-merge');
-
-gulp.task('yaml', function() {
-  return gulp.src(['./_config/_dev.yml','./_config/build/*.yml'])
-    .pipe(yamlMerge('_config.yml'))
-    .pipe(gulp.dest('./_dev/'));
-});
